a2ed1a167065bf8fbf91ca8e04bf24d9bb5983dc
Daniel Rosas Mialma <dan11_daniel@hotmail.com>
2021-03-16 16:11:41 -0600

    Arreglar el trabajar lo usuarios

---
+++ AuthorizationBlazorServer/Server/Services/IdentityServerBlazorBuilderExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationBlazorServer.Server.Services
+{
+    public static class IdentityServerBlazorBuilderExtension
+    {
+        /// <summary>
+        /// Adds test users.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="users">The users.</param>
+        /// <returns></returns>
+        public static IIdentityServerBuilder AddUsers(this IIdentityServerBuilder builder, Action<DbContextOptionsBuilder> options)
+        {
+            builder.Services.AddDbContext<UserStore>(options);
+            builder.AddProfileService<UserProfileService>();
+            builder.AddResourceOwnerValidator<UserResourceOwnerPasswordValidator>();
+
+            return builder;
+        }
+    }
+}
---
+++ AuthorizationBlazorServer/Server/Services/User.cs
@@ -0,0 +1,24 @@
+﻿using IdentityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthorizationBlazorServer.Server.Services
+{
+    /// <summary>
+    /// Manejar contexto de usuarios personalizado basado en TestUser de IdentityServer4
+    /// </summary>
+    public class User
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ExternalName { get; set;}
+        public string ExternalId { get; set; }
+        public bool IsActive { get; set; }
+        public ICollection<Claim> Claims { get; set; } = new HashSet<Claim>(new ClaimComparer());
+
+    }
+}
